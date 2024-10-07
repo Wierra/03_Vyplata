f3cd1bae2b85b7c68b541bbfbe6e799632949304
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-07 18:56:57 +0200

    Doplnění02

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hodinaSazba = Number(prompt("Zadej svou hodinovou sazbu v Kč."))
-const doba = 8
-const delka = 21
+const doba = Number(prompt("Zadej počet hodin za den", "8"))
+const delka = Number(prompt("Zadej počet odpracovaných dní v měsíci." , "21"))
 
 const mzda = hodinaSazba * doba * delka
 
